da32cc3da91533275e438c89b869a698ac2bc0e9
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2018-08-25 23:49:38 +0200

    Split search into two pages and use a form with GET parameters. Enables URL sharing and the back button.

--- js/index.js
+++ js/index.js
@@ -51,8 +51,9 @@ function search(indexMap,decklists,cardNames)
 // deckHits = [...deckHits.entries()].sort((a,b)=>decklists[a[0]].tier>decklists[b[0]].tier);
  deckHits = [...deckHits.entries()].sort((a,b)=>a[1].length<b[1].length);
  const rows = deckHits.map(e=>`<tr><td><a href="${e[0]}">${decklists[e[0]].name}</a></td><td>${decklists[e[0]].tier}</td><td>${e[1].length}</td><td>${e[1]}</td></tr>\n`);
- document.write("<table>");
- document.write("<tr><th>Decklist</th><th>Tier</th><th>Hits</th><th>Cards</th></tr>");
- for(const row of rows) document.write(row);
- document.write("</table>");
+ const table = document.getElementById("resulttable");
+ for(const row of rows)
+ {
+   table.innerHTML+=row;
+ }
 }
